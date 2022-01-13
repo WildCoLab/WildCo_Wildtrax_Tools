d01f6454a747b897577155bb30e8bb0e1b07f946
Laura Stewart <lastew@example.com>
2022-01-13 16:35:50 -0700

    update to final ARU

--- Data-massaging-ARU.R
+++ Data-massaging-ARU.R
@@ -9,42 +9,41 @@ library(dplyr)
 library(tidyr)
 library(stringr)
 
-setwd("C:/Users/laura/Documents/Wildco/3. Data and scripts/4. Raw data downloads")
+version<-"v2"
 
-dat1<-read.csv("ECCC_Boreal_Monitoring_Strategy_CWS_Northern_Region_2020_-_Tuyeta_report.csv")
-dat2<-read.csv("ECCC_Tuyeta_PA_CWS_Northern_Region_2020_report.csv")
+setwd("C:/Users/laura/Documents/Wildco/3. Data and scripts/1. Master data")
+
+dat1<-read.csv("../4. Raw data downloads/ECCC_Boreal_Monitoring_Strategy_CWS_Northern_Region_2020_-_Tuyeta_report.csv")
+dat2<-read.csv("../4. Raw data downloads/ECCC_Tuyeta_PA_CWS_Northern_Region_2020_report.csv")
 dat2$location_2<-str_pad(dat2$location_2,2,side="left",pad="0")
 dats<-rbind(dat1,dat2)
 
 str(dats)
-nchar(dats$location_3)
 dats$location_3<-str_pad(dats$location_3,3,side = "left",pad="0")
 dats$location_4<-str_pad(dats$location_4,2,side="left",pad="0")
-dats$location<-str_c(dats$location_1,dats$location_2,dats$location_3,dats$location_4,sep="-")
+dats$Location<-str_c(dats$location_1,dats$location_2,dats$location_3,dats$location_4,sep="-")
+head(dats$Location)
 
 
+cov<-read.csv("Tuyeta_Station_Covariates.csv")
+latlongs<-cov[,c("Location","Latitude","Longitude")]
+head(latlongs)
 
-
-
-# change this here. Need to make a station covariates sheet rather than glean it
-# from this like a n00b
-
-camdat<-read.csv("Tuyeta_detection_data_v3.csv")
-latlongs<-camdat[,c("location","Latitude","Longitude")]
-latlongs<-unique(latlongs)
-
-newloc<-c("BMS-FMP-002-20", "TTEA-01-6084-01") #locations that don't have cameras
-newlat<-c(66.24281981, 65.604273 )
-newlong<-c(-129.3581903,-129.695867)
-newlocs<-as.data.frame(cbind(newloc,newlat,newlong))
-names(newlocs)<-names(latlongs)
-latlongs<-rbind(latlongs,newlocs)
-
-latlongs<-unique(latlongs)
 
 dats<-subset(dats, select = -c(latitude, longitude))
-birddata<-merge(latlongs,dats, all.y=T)
+birddata<-merge(latlongs,dats, all.y=T, by = "Location")
 birddata$Latitude<-as.numeric(birddata$Latitude)
 summary(birddata$Latitude)
 
-write.csv(birddata,"Tuyeta_ARU_Data_v1.csv",row.names = F)
+birddata2=birddata%>%
+  select(-project,-organization,-status,-buffer_radius_m, -scientific_name,
+         -daily_weather_station_nm,-daily_weather_station_elevation,-daily_weather_station_distance,
+         -daily_min_temp,-daily_max_temp,-daily_mean_temp,-daily_total_rain_mm,
+         -daily_total_snow_cm,-daily_precipitation_mm,-daily_snow_on_ground_cm,
+         -hourly_weather_station_nm,-hourly_weather_station_elevation,-hourly_weather_station_distance,
+         -hourly_temp,-hourly_dew_point,-hourly_rel_humidity,-hourly_station_pressure,
+         -hourly_humidex,-hourly_wind_chill,-hourly_wind_direction,-hourly_weather_attributes,
+         -hourly_precipitation_mm,-hourly_wind_speed,-hourly_visibility_km,-land_features,
+         -equipment_used,-tagged_in_wildtrax,-location,-location_1,-location_2,-location_3,-location_4)
+
+write.csv(birddata,paste0("Tuyeta_ARU_Data_", version, ".csv"),row.names = F)
