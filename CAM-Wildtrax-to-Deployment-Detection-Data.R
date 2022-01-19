b8114b30147dca159bd1ccaefa84d9dfd36d9e81
Laura Stewart <lastew@example.com>
2022-01-19 14:23:38 -0700

    finish group count

--- CAM-Wildtrax-to-Deployment-Detection-Data.R
+++ CAM-Wildtrax-to-Deployment-Detection-Data.R
@@ -40,7 +40,7 @@ data$field_of_view<-as.factor(data$field_of_view)
 data$common_name<-as.factor(data$common_name)
 data$age_class<-as.factor(data$age_class)
 data$sex<-as.factor(data$sex)
-data$number_individuals<-as.numeric(data$number_individuals) # as NUMERIC this time
+data$count<-as.numeric(data$count) # as NUMERIC this time
 data$field_of_view<-as.factor(data$field_of_view)
 summary(data$field_of_view)
 
@@ -155,6 +155,8 @@ names(data)[13]
 names(data)[13]<-"age"
 names(data)[15]
 names(data)[15]<- "count"
+data$age[data$age == "VNA"] <- NA
+data$sex[data$sex == "VNA"] <- NA
 
 lessdata = data %>%
   select(-project, - organization, -buffer_radius_m, -scientific_name, -daily_weather_station_nm,
@@ -205,79 +207,81 @@ if(dat$duration[nrow(dat)] < (mins * 60)|
 }
 
 
-# This is where we need the Minimum.Group.Size
+# Minimum group size at the tag level (includes age and sex)
 
-dat$tag_comments
-group<-str_match(dat$tag_comments, "gc")
-group<-as.factor(group)
-summary(group)
-head(group)
-dat$tag_comments[! is.na(group)]
-
+tpos<-str_locate(dat$tag_comments, "gc")[,2]
+tmptag=str_sub(dat$tag_comments, start = tpos+1, end = tpos+3)
+tendpos=str_locate(tmptag,"\\D")[,2]
+tendpos[is.na(tendpos)]<-2
+dat$groupcount_tag<- str_sub(tmptag,start = 1, end = tendpos-1)
 
 
+# Minimum group size by species, regardless of age and sex
 
+pos<-str_locate(dat$image_comments, "group count = ")[,2]
+tmp=str_sub(dat$image_comments, start = pos+1, end = pos+3)
 
-
-
-
-
-
-
-# And Minimum.Species.Group.Size
-
-
-demcomments<-dat$image_comments[str_detect(dat$image_comments, "group count = ")]
-str_locate(demcomments, "group count = ")
-pos<-str_locate(demcomments, "group count = ")[,2]
-str_sub(demcomments, start = pos+1, end = pos+3)
-tmp=str_sub(demcomments, start = pos+1, end = pos+3)
-tmp[390]<-paste0(tmp[390],", 2")
-tmp
 endpos=str_locate(tmp,"\\D")[,2]
 endpos[is.na(endpos)]<-2
-str_sub(tmp,start = 1, end = endpos-1)
+dat$groupcount_species <-str_sub(tmp,start = 1, end = endpos-1)
 
 
 
+# Now collapse the thing into independent events
+class(dat$count)
+
+as.data.frame(dat[dat$Event.ID == "E1000039",])
+
+events<-dat %>%
+  dplyr::group_by(Event.ID, common_name, age, sex) %>%
+  dplyr::summarise (gc_species = max(groupcount_species, na.rm = T),
+                    gc_tag = max(groupcount_tag, na.rm = T),
+                    gc_regular = max(count, na.rm = T))
+events$gc_regular[events$gc_regular == -Inf]<-NA
+
+for (i in 1: nrow(events)){
+  if (is.na(events$gc_tag [i])){
+    events$gc_tag[i] <- events$gc_regular[i]
+  }
+}
+events$gc_tag<-as.numeric(events$gc_tag)
+
+species_only<- events %>%
+  group_by(Event.ID, common_name) %>%
+  summarise(gc_estimated = sum(gc_tag, na.rm = T))
+
+events2<-merge(events, species_only, by = c("Event.ID","common_name"),all.y=T)
+
+events2 = events2 %>% mutate(mistake = events2$gc_estimated > events2$gc_species)
 
 
-
-
-
-
+for (i in 1: nrow(events2)){
+  if (is.na(events2$gc_species [i])){
+    events2$gc_species[i] <- events2$gc_estimated[i]
+  }
+}
 
 # Calculate the event length and size 
 
 # find out the last and the first of the time in the group
 top <- dat %>% group_by(Event.ID) %>% top_n(1,date_detected) %>% select(Event.ID, date_detected)
 bot <- dat %>% group_by(Event.ID) %>% top_n(-1,date_detected) %>% select(Event.ID, date_detected)
-names(bot)[2] <- c("Date_Time.Captured_end")
-dec_no <- dat %>% group_by(Event.ID) %>% summarise(n())
-event_grp <- dat %>% group_by(Event.ID) %>% summarise(max(Minimum.Group.Size))
+names(bot)[2] <- c("date_detected_end")
+dec_no <- dat %>% group_by(location, Event.ID) %>% summarise(n())
+names(dec_no)[3]<-"number_images"
 
 # calculate the duration
 diff <-  top %>% left_join(bot, by="Event.ID") %>%
-  mutate(duration=abs(int_length(Date_Time.Captured %--% Date_Time.Captured_end))) %>%
-  left_join(event_grp, by="Event.ID")%>%
+  mutate(duration=abs(int_length(date_detected %--% date_detected_end))) %>%
   left_join(dec_no, by="Event.ID")
 
 # Remove duplicates
 diff <- diff[duplicated(diff)==FALSE,]
 
-names(diff) <- c("Event.ID","Date_Time.end","Date_Time.start","Event.Duration","Event.Groupsize","Event.Observations")
-diff$Date_Time.end<-NULL;diff$Date_Time.start<-NULL
-dat$duration <-NULL
 # Merge the data
-dat <-  dat %>%
-  left_join(diff,by="Event.ID")
+independent_data <-  merge(diff, events2, by = "Event.ID", all.y = T)
 
-# Subset to the first observation in each event
 
-# Subset to 30 minute indepedenents
-ind.dat <- dat[!duplicated(dat$Event.ID),]
-ind.dat <- as.data.frame(ind.dat)
-ind.dat$Species <-as.factor(ind.dat$Species)
 
 # Save it for a rainy day
-write.csv(ind.dat, paste0(dat$Project.ID[1], "_",independent ,"min_Independent.csv"), row.names = F)
+write.csv(independent_data, paste0("Tuyeta_independent_",version, ".csv"), row.names = F)
